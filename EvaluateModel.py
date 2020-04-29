1. Testing model 2. Fix rotations: kornia expects angles, not radians

EvaluateModel.py
@@ -1,5 +1,6 @@
 import torch as ch
 from Model_Implementations import Model_Tanh_Ensemble, predict
+from cleverhans.future.torch.attacks import projected_gradient_descent
 import scipy.linalg
 import numpy as np
 from tqdm import tqdm
@@ -12,7 +13,7 @@ M = ch.load('models/M.pt').cuda()
 # Load model
 model = Model_Tanh_Ensemble(M=M)
 model = ch.nn.DataParallel(model).cuda()
-model.load_state_dict(ch.load('models/checkpoint_25.pth'))
+model.load_state_dict(ch.load('models/checkpoint_300.pth'))
 
 # Switch to eval model
 model.eval()
@@ -22,15 +23,46 @@ constants = utils.CIFAR10()
 ds = constants.get_dataset()
 num_examples = 0
 acc = 0.0
-_, test_loader = ds.make_loaders(batch_size=128, workers=8, data_aug=False, only_val=True)
+_, test_loader = ds.make_loaders(batch_size=32, workers=8, data_aug=False, only_val=True)
 iterator = tqdm(test_loader)
 for im, label in iterator:
 	im    = im.cuda()
 	label = label.cuda()
 
-	# Encode labels
+	# Get model predictions
 	y_p   = predict(model, im)
 	preds = ch.argmax(y_p, 1)
 	num_examples += label.shape[0]
 	acc += (ch.sum(preds == label)).float()
 	iterator.set_description('Accuracy : %.3f' % (100 * acc / num_examples))
+
+
+class ECOCToNormal:
+	def __init__(self, m):
+		self.m = m
+
+	def __call__(self, x):
+		logits = predict(self.m, x)
+		return logits
+
+wrapped_model = ECOCToNormal(model)
+# eps = 8/255
+# norm = np.inf
+eps = 0.5
+norm = 2
+nb_steps = 20
+eps_iter = 2.5 * eps / nb_steps
+misclass, total = 0, 0
+
+_, test_loader = ds.make_loaders(batch_size=32, workers=8, data_aug=False, only_val=True)
+iterator = tqdm(test_loader)
+# Run adversarial evaluation
+for im, label in iterator:
+	# Run PGD attack
+	advs = projected_gradient_descent(wrapped_model, im.cuda(), eps, eps_iter, nb_steps, norm, 0, 1)
+	pert_labels = ch.argmax(wrapped_model(advs), 1).cpu()
+
+	misclass += (pert_labels != label).sum().item()
+	total    += len(label)
+
+	iterator.set_description('Attack success rate : %f' % (100 * misclass / total))

Model_Implementations.py
@@ -13,7 +13,7 @@ class ClassBlender(nn.Module):
     def forward(self, x):
         if self.training:
             x_permuted = x[ch.randperm(x.shape[0])]
-            angles = ((180 * ( 2 * ch.rand(x.shape[0]) - 1)) * np.pi / 180).to(x.get_device())
+            angles = ((180 * ( 2 * ch.rand(x.shape[0]) - 1))).to(x.get_device())
             shifts = (4 * (2 * ch.rand(x.shape[0], 2) -1)).to(x.get_device())
             inputs_permuted_translated = kornia.translate(x_permuted, shifts)
             x_adjusted = kornia.rotate(inputs_permuted_translated, angles)
@@ -28,7 +28,7 @@ class DataAugmenter(nn.Module):
 
     def forward(self, x):
         if self.training:
-            angles = ((15 * (2 * ch.rand(x.shape[0]) - 1)) * np.pi / 180).to(x.get_device())
+            angles = ((15 * (2 * ch.rand(x.shape[0]) - 1))).to(x.get_device())
             shifts = (4 * (2 * ch.rand(x.shape[0], 2) - 1)).to(x.get_device())
             inputs_shifted = kornia.translate(x, shifts)
             inputs_shifted_rotated = kornia.rotate(inputs_shifted, angles)
@@ -184,15 +184,14 @@ class Model_Tanh_Ensemble(nn.Module):
         return ch.stack(outputs).permute(1, 0, 2)
 
 def predict(model, x):
-    with ch.no_grad():
-        outputs = model(x)
-        # Reshuffle outputs back to actual shape
-        outputs = outputs.permute(1, 0, 2)
-        # Run activation function on activations
-        outputs = ch.tanh(outputs)
-        outputs = [o for o in outputs]
-        outputs = ch.cat(outputs, dim=-1)
-        outputs = ch.mm(outputs, ch.t(model.module.M))
-        # Log-ReLU
-        logits = ch.log(relu(outputs) + 1e-6)
-        return logits
+    outputs = model(x)
+    # Reshuffle outputs back to actual shape
+    outputs = outputs.permute(1, 0, 2)
+    # Run activation function on activations
+    outputs = ch.tanh(outputs)
+    outputs = [o for o in outputs]
+    outputs = ch.cat(outputs, dim=-1)
+    outputs = ch.mm(outputs, ch.t(model.module.M))
+    # Log-ReLU
+    logits = ch.log(relu(outputs) + 1e-6)
+    return logits